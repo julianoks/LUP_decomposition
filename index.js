330170aaa3b6220c0458ccb9460528a37fb15b32
julianoks <juao15@hampshire.edu>
2019-01-26 20:49:28 -0500

    LUP Solve

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ function swap(arr, idx1, idx2){
     arr[idx2] = temp
 }
 
+/**
+ * Performs an LUP decomposition on A, in-place.
+ * After execution `A` will contain the matrices L,U.
+ * @param {Number[][]} A An nxn matrix. Note that `A` is updated in-place.
+ * @returns {Number[]} The permutation.
+ * 
+ * @example
+ * // Logs [[3,0,2,1],[[5,-4,-3,1],[0.4,5.6,2.2,-3.4],[0.8,0.9285714285714287,-2.642857142857143,7.357142857142858],[-0.2,-0.5,-0.9459459459459459,9.45945945945946]]]
+ * let test1 = [[2,4,1,-3],[-1,-2,2,4],[4,2,-3,5],[5,-4,-3,1]];
+ * const pi = LUPDecomposition(test1);
+ * console.log([pi, test1)];
+ */
 function LUPDecomposition(A){
     const n = A.length;
     const pi = Array(n).fill().map((_,i) => i);
@@ -26,9 +38,35 @@ function LUPDecomposition(A){
             }
         }
     }
-    return [A,pi];
+    return pi;
 }
 
-/*
-LUPDecomposition([[2,4,1,-3],[-1,-2,2,4],[4,2,-3,5],[5,-4,-3,1]])
-*/
+
+/**
+ * Solves Ax=b for the unknown vector x.
+ * @param {Number[][]} A An nxn matrix.
+ * @param {Number[]} b A vector of length n.
+ * 
+ * @example
+ * // returns something close to [21, 5, -1]
+ * LUPSolve([[-2,8,-5],[3,-11,7],[9,-34,21]], [3,1,-2])
+ */
+function LUPSolve(A, b){
+    // modifies A in-place to contain matrices L,U
+    const pi = LUPDecomposition(A);
+    const n = A.length;
+    // Solve Ly=Pb for the unknown y=UX using forward substitution
+    let y = Array(n).fill();
+    for(let i=0; i<n; i++){
+        const sum = y.slice(0, i)
+            .reduce((a, y_j, j) => a + (y_j * A[i][j]), 0);
+        y[i] = b[pi[i]] - sum;
+    }
+    // Solve Ux=y for the unknown x using back substitution
+    let x = Array(n).fill();
+    for(let i=n-1; i>=0; i--){
+        const sum = x.slice(i+1).reduce((a, x_j, j) => a + (x_j * A[i][j+i+1]), 0);
+        x[i] = (y[i] - sum) / A[i][i];
+    }
+    return x;
+}
